bd6077ed9d0a7ee7bab831e6b08d47d4b08e47c3
MiguelSuarez-hub <miguel.suarezf05@gmail.com>
2024-09-28 23:13:28 -0500

    Change: astro Image for img

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,10 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 
 import tailwind from "@astrojs/tailwind";
-import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()],
+  integrations: [tailwind()],
   output: 'server',
   adapter: vercel(),
 });

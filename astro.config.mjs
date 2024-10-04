8d087ddcb48a727a5a3fc85ddc5b622bc4389b36
MiguelSuarez-hub <miguel.suarezf05@gmail.com>
2024-10-03 23:22:01 -0500

    Added i18n for the navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,30 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
-
+import { i18n, filterSitemapByDefaultLocale } from "astro-i18n-aut/integration";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+
+const defaultLocale = "en";
+const locales = {
+  en: "en-US", // the `defaultLocale` value must present in `locales` keys
+  es: "es-CO",
+};
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  trailingSlash: "always",
+  integrations: [i18n({
+    locales,
+    defaultLocale,
+  }), 
+  sitemap({
+    i18n: {
+      locales,
+      defaultLocale,
+    },
+    filter: filterSitemapByDefaultLocale({ defaultLocale }),
+  }),
+  tailwind()],
   output: 'server',
   adapter: vercel(),
 });
